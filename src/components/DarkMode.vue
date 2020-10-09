a8e382cf6c8c25ba069256da871e608254822231
thomas <thomas@takotak.pro>
2020-10-09 08:27:00 +0200

    refacto colors

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -8,7 +8,7 @@
     export default {
         methods: {
             toggleTheme() {
-
+                document.documentElement.classList.toggle('darkmode')
             }
         }
     }
@@ -16,9 +16,15 @@
 
 <style lang="scss">
     :root {
+        --light: #fff;
+        --text: #2c3e50;
+        --link: #42b983;
 
+        background: var(--light);
+        transition: background-color 0.2s ease;
     }
     :root.darkmode {
-
+        --light: #080913;
+        --text: #dedede;
     }
 </style>
